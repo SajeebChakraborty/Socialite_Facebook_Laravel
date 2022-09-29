a8b157152194e15b19cde5d08d724f0942dcfeaa
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-09-29 20:25:50 +0600

    Version 9.1

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -1,14 +1,13 @@
 <?php
-
+  
 namespace App\Http\Controllers;
-
+  
 use Illuminate\Http\Request;
-
 use Laravel\Socialite\Facades\Socialite;
 use Exception;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-
+  
 class FacebookController extends Controller
 {
     /**
@@ -20,7 +19,7 @@ class FacebookController extends Controller
     {
         return Socialite::driver('facebook')->redirect();
     }
-          
+           
     /**
      * Create a new controller instance.
      *
@@ -33,28 +32,29 @@ class FacebookController extends Controller
             $user = Socialite::driver('facebook')->user();
          
             $finduser = User::where('facebook_id', $user->id)->first();
-        
+         
             if($finduser){
          
                 Auth::login($finduser);
-        
+       
                 return redirect()->intended('dashboard');
          
             }else{
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'facebook_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
-                ]);
+                $newUser = User::updateOrCreate(['email' => $user->email],[
+                        'name' => $user->name,
+                        'facebook_id'=> $user->id,
+                        'password' => encrypt('123456dummy')
+                    ]);
         
                 Auth::login($newUser);
         
                 return redirect()->intended('dashboard');
             }
-        
+       
         } catch (Exception $e) {
             dd($e->getMessage());
         }
     }
 }
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
   
 use Illuminate\Support\Facades\Route;
   
-use App\Http\Controllers\FacebookController;
+use App\Http\Controllers\GoogleController;
   
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +15,15 @@ use App\Http\Controllers\FacebookController;
 |
 */
   
-Route::get('auth/facebook', [FacebookController::class, 'redirectToFacebook']);
-Route::get('auth/facebook/callback', [FacebookController::class, 'handleFacebookCallback']);
+Route::get('/', function () {
+    return view('welcome');
+});
+  
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+  
+Route::controller(FacebookController::class)->group(function(){
+    Route::get('auth/facebook', 'redirectToFacebook')->name('auth.facebook');
+    Route::get('auth/facebook/callback', 'handleFacebookCallback');
+});
